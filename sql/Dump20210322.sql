9e1f259822ea2f15ea80dd699d5ca4f54f5f67a1
toastnjamz <veritydavis@gmail.com>
2021-04-01 19:14:05 -0600

    Adding SQL dump file

---
+++ sql/Dump20210322.sql
@@ -0,0 +1,67 @@
+-- MySQL dump 10.13  Distrib 8.0.19, for macos10.15 (x86_64)
+--
+-- Host: 127.0.0.1    Database: patients
+-- ------------------------------------------------------
+-- Server version	8.0.19
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `patient`
+--
+
+DROP TABLE IF EXISTS `patient`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `patient` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `given` varchar(30) NOT NULL,
+  `family` varchar(30) NOT NULL,
+  `dob` datetime NOT NULL,
+  `sex` char(1) NOT NULL,
+  `address` varchar(45) DEFAULT NULL,
+  `phone` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `patient`
+--
+
+LOCK TABLES `patient` WRITE;
+/*!40000 ALTER TABLE `patient` DISABLE KEYS */;
+INSERT INTO `patient` VALUES
+(1,'Lucas','Ferguson','1968-06-22 00:00:00','M','2 Warren Street','387-866-1399'),
+(2,'Pippa','Rees','1952-09-27 00:00:00','F','745 West Valley Farms Drive','628-423-0993'),
+(3,'Edward','Arnold','1952-11-11 00:00:00','M','599 East Garden Ave','123-727-2779'),
+(4,'Anthony','Sharp','1946-11-26 00:00:00','M','894 Hall Street','451-761-8383'),
+(5,'Wendy','Ince','1958-06-29 00:00:00','F','4 Southampton Road','802-911-9975'),
+(6,'Tracey','Ross','1949-12-07 00:00:00','F','40 Sulphur Springs Dr','131-396-5049'),
+(7,'Claire','Wilson','1966-12-31 00:00:00','F','12 Cobblestone St','300-452-1091'),
+(8,'Max','Buckland','1945-06-24 00:00:00','M','193 Vale St','833-534-0864'),
+(9,'Natalie','Clark','1964-06-18 00:00:00','F','12 Beechwood Road','241-467-9197'),
+(10,'Piers','Bailey','1959-06-28 00:00:00','M','1202 Bumble Dr','747-815-0557');
+/*!40000 ALTER TABLE `patient` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-03-22 11:43:11
